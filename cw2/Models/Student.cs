3917013420eb016aef0f58678b89ab996baf97e4
s19542 <s19542@pjwstk.edu.pl>
2020-04-19 00:15:40 +0200

    The 3th commit

---
+++ cw2/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cw2.Models
+{
+    class Student
+    {
+    }
+}
--- cw2/Program.cs
+++ cw2/Program.cs
@@ -7,33 +7,57 @@ namespace cw2
 {
     class Program
     {
-       
+        private static string pathForExceptions = @"C:\Users\Алиса\Desktop\cw2\cw2\Data\log.txt";
 
         static void Main(string[] args)
         {
-            var exePath = AppDomain.CurrentDomain.BaseDirectory;//path to exe file
-            var path = Path.Combine(exePath, "Data\\dane.csv");
 
-           // string pathOfExeptionFile = @"C:\Users\Алиса\Desktop\cw2\cw2\Data\dane.csv";
+            string pathForData = @"C:\Users\Алиса\Desktop\cw2\cw2\Data\dane.csv";
+
+
             Console.WriteLine("Please enter the path of: \n-source file,\n-result file, \n-format of data;");
+
             string[] data = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            Console.WriteLine(data.Length);
-
-           // string path = @"Data\dane.csv";
-            Console.WriteLine("Hello World");
-
-            //Wczytywanie 
-            var fi = new FileInfo(path);
-            using (var stream = new StreamReader(fi.OpenRead()))
+            if (data.Length == 3)
             {
-                string line = null;
-                while ((line = stream.ReadLine()) != null)
+
+                if (new FileInfo(@data[0]).Exists)
                 {
-                    string[] kolumny = line.Split(',');
-                    Console.WriteLine(line);
+
+                    if (new FileInfo(@data[0]).Extension.Contains("csv"))
+                    {
+                      
+                        ThrowException($"The file {data[0]} has incorrect extension!!!");
+                        data[0] = @"C:\Users\Алиса\Desktop\cw2\cw2\Data\dane.csv";
+                    }
+
                 }
+                else
+                {
+                    ThrowException($"The file {data[0]} does not exist!!!");
+                    data[0] = @"C:\Users\Алиса\Desktop\cw2\cw2\Data\dane.csv";
+
+                }
+
+                if (!new FileInfo(@data[1]).Exists)
+                {
+                  
+                    ThrowException($"The file {data[1]} does not exist!!!");
+                    data[1] = @"C:\Users\Алиса\Desktop\cw2\cw2\Data\result.xml";
+
+                }
+
+            }
+            else
+            {
+              
+                ThrowException("The number of arguments is incorrect!!!");
             }
 
+
+            //Wczytywanie  z csv
+            ReadFromFile(@data[0]);
+           
 
 
 
@@ -43,5 +67,42 @@ namespace cw2
         {
 
         }
+
+
+        static async void ThrowException(string text)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(pathForExceptions, false, System.Text.Encoding.Default))
+                {
+                    await sw.WriteLineAsync(text);
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void ReadFromFile(string pathForData)
+        { 
+            var fi = new FileInfo(pathForData);
+
+            using (var stream = new StreamReader(fi.OpenRead()))
+            {
+                string line = null;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    string[] kolumny = line.Split(',');
+                    Console.WriteLine(line);
+                }
+
+
+
+            }
+
+        } 
     }
 }
