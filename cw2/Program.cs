cbcb974a4ee4615c09e59ebcac27e9a9eeff8f6d
s19542 <s19542@pjwstk.edu.pl>
2020-04-18 22:32:55 +0200

    The 2th commit

--- cw2/Program.cs
+++ cw2/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace cw2
 {
     class Program
     {
+       
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var exePath = AppDomain.CurrentDomain.BaseDirectory;//path to exe file
+            var path = Path.Combine(exePath, "Data\\dane.csv");
+
+           // string pathOfExeptionFile = @"C:\Users\Алиса\Desktop\cw2\cw2\Data\dane.csv";
+            Console.WriteLine("Please enter the path of: \n-source file,\n-result file, \n-format of data;");
+            string[] data = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(data.Length);
+
+           // string path = @"Data\dane.csv";
+            Console.WriteLine("Hello World");
+
+            //Wczytywanie 
+            var fi = new FileInfo(path);
+            using (var stream = new StreamReader(fi.OpenRead()))
+            {
+                string line = null;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    string[] kolumny = line.Split(',');
+                    Console.WriteLine(line);
+                }
+            }
+
+
+
+
+
+        }
+        static void Converter(string _scvData = "data.csv", string _wynik = "result.xml", string format = "xml")
+        {
+
         }
     }
 }
